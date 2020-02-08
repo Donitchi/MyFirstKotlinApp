9a13a294ef24f3347afd1887c0ae22053133719d
Doni Phan <donitsi@Doni-MacBook.local>
2020-02-08 15:31:11 +0200

    Added a new assignment for Andy

--- MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
+++ MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: Exercise 1 -> Add a new button. Button will show a Toast message "Button clicked!"
     }
 }
