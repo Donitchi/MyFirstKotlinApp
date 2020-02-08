0d6346b964d616ef34e3ef9284cf1c7fef45df2d
Doni Phan <donitsi@Doni-MacBook.local>
2020-02-08 15:43:23 +0200

    Added more useful links

--- MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
+++ MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: Exercise 1 -> Add a new button. Button will show a Toast message "Button clicked!"
+        // TODO: Exercise 1 -> Add a new button. Button will show a Toast message "Button clicked!"x1
     }
 }
